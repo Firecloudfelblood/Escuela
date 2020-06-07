958e6adcf5a9953b150f7362652381ba043a3a32
Cavazos, Gyovanny <gyovanny.cavazos@thermofisher.com>
2020-06-06 19:11:23 -0700

    menu principal en loop

--- src/com/erivalaxl/Main.java
+++ src/com/erivalaxl/Main.java
@@ -1,4 +1,11 @@
 package com.erivalaxl;
 
 public class Main {
+    public static void main(String[] args) {
+        Menus menu = new Menus();
+
+               menu.menuPrincipal();
+        System.out.println(" Gracias por usar mi aplicacion! :) ");
+
+    }
 }
